8e9f33406696391615cdf73cbe4074883808aaf0
a r i <91373874+zigaz23@users.noreply.github.com>
2021-12-05 04:04:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -21,5 +21,5 @@ contact_filter = filters.create(
     or message.outgoing
 )
 
-bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicVideo"))
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Zaen"))
 call_py = PyTgCalls(bot)
