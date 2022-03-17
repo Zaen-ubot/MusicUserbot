193a15f09efa1727f726da084843818ba5948a4f
ZaenProjec <91957490+Zaen-ubot@users.noreply.github.com>
2022-03-17 18:39:44 +0700

    Update Zaen.py

--- ZaenHelper/Zaen.py
+++ ZaenHelper/Zaen.py
@@ -1,7 +1,7 @@
 from random import randint
 from pyrogram import Client, filters
 from config import HNDLR, bot as USER
-from Zaen.helpers.decorators import authorized_users_only
+from ZaenHelper.helpers.decorators import authorized_users_only
 from pyrogram.errors import UserAlreadyParticipant
 from pyrogram.raw.functions.phone import CreateGroupCall
 
