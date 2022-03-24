ee7a1cdd6158641df035ba183a8b8695d9db2382
ZaenProjec <91957490+Zaen-ubot@users.noreply.github.com>
2022-03-24 19:55:51 +0700

    .

--- Zaen/Zaen.py
+++ Zaen/Zaen.py
@@ -4,7 +4,6 @@ from config import HNDLR, bot as USER
 from Zaen.helpers.decorators import authorized_users_only
 from pyrogram.errors import UserAlreadyParticipant
 from pyrogram.raw.functions.phone import CreateGroupCall
-from pytgcalls import GroupCall
 from pytgcalls import GroupCallFactory
 
 @Client.on_message(filters.command(["join"], prefixes=f"{HNDLR}"))
