a375127140c32f238975ecd9531cce9beb56aee8
ZaenProjec <91957490+Zaen-ubot@users.noreply.github.com>
2022-06-09 13:24:55 +0700

    .

--- MusicUserbot/userbot.py
+++ MusicUserbot/userbot.py
@@ -44,7 +44,7 @@ async def ping(client, m: Message):
    end = datetime.now()
    uptime_sec = (current_time - START_TIME).total_seconds()
    uptime = await _human_time_duration(int(uptime_sec))
-   await m_reply.edit(f"🏓𝗣𝗼𝗻𝗴!!\n𝗦𝗽𝗲𝗲𝗱  - {delta_ping * 1000:.3f} ms \n𝗨𝗽𝘁𝗶𝗺𝗲 - {uptime}")
+   await m_reply.edit(f"**𝗣𝗼𝗻𝗴!!🏓**\n**Pinger**  - {delta_ping * 1000:.3f} ms \n**Uptime** - {uptime}")
 
 
 @Client.on_message(filters.command(["kping"], prefixes=f"{HNDLR}"))
